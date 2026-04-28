2a4ae394001f0291292e729edaca466f6885a0ce
PriosMilky <pahrulhk11@gmail.com>
2026-04-28 17:40:11 +0700

    materi_satu

---
+++ materi.py
@@ -0,0 +1,21 @@
+nama = "Pahrul Hidayat Koswara"
+usia = 24
+
+# cara print out di py
+print("----------------------")
+print(nama)
+print(usia)
+
+print("----------------------")
+
+print(f"{nama}")
+print(f"{usia}")
+
+print("----------------------")
+
+print(f'''
+Nama Saya adalah {nama}
+Usia saya adalah {usia}
+      ''')
+
+print("----------------------")
---
+++ pertemuan1.py
@@ -0,0 +1,30 @@
+import random
+
+welcome_messege = "WELCOME TO CUYPY GAMES!!"
+cuypy_position = random.randint(1,4)
+
+print("==============================")
+print(f"== {welcome_messege} ==")
+print("==============================")
+
+user_name = input("INPUT NAME: ")
+    
+print(f'''
+Hello {user_name}, see the cave below
+[] [] [] []
+''')
+
+
+while True:
+    user_input = int(input("Which cave do you think Cuypy is in? [1 / 2 / 3 / 4]: "))
+    
+    confirm = input(f"ARE YOU SURE CHOSE THE NUMBER {user_input}? [y/n]")
+    
+    if confirm == "y":
+        if user_input == cuypy_position:
+            print(f"CONGRATULATIONS {user_name} YOU WIN, , CUYPY POSITION IN {cuypy_position}")
+        else:
+            print(f"NO YOU LOSE, , CUYPY POSITION IN {cuypy_position} AND YOU SELECTED IS {user_input}")
+            break
+    else:
+        print("PLEASE REPEAT AGAIN")
